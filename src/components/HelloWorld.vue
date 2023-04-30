689cf2331bb5c8df1c3a74ed47666c48dffa0d30
Liam929 <1853435@tongji.edu.cn>
2023-04-30 20:44:51 +0800

    second commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,172 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="app">
+    <header>
+      <nav>
+        <ul>
+          <li v-for="(page, index) in pages" :key="index" :class="{ active: currentPage == index }" @click="currentPage = index">
+            {{ page.name }}
+            <ul v-if="page.children" class="sub-menu">
+              <li v-for="(child, childIndex) in page.children" :key="childIndex" @click="goToChildPage(child)">
+              {{ child.name }}
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <h1>{{ pages[currentPage].name }}</h1>
+      <p>{{ pages[currentPage].description }}</p>
+      <div v-if="childPage">
+        <h2>{{ childPage.name }}</h2>
+        <p>{{ childPage.description }}</p>
+      </div>
+    </main>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  export default {
+    data() {
+      return {
+        currentPage: 0,
+        childPage: null,
+        pages: [
+          {
+            name: "Attack TTPs",
+            description: "This is the first page",
+            children: [
+              {
+                name: "Sub-page 1",
+                description: "This is a sub-page of Page 1"
+              },
+              {
+                name: "Sub-page 2",
+                description: "This is another sub-page of Page 1"
+              }
+            ]
+          },
+          {
+            name: "bar2",
+            description: "This is the second page",
+            children: [
+              {
+                name: "Sub-page 1",
+                description: "This is a sub-page of Page 2"
+              },
+              {
+                name: "Sub-page 2",
+                description: "This is another sub-page of Page 2"
+              }
+            ]
+          },
+          {
+            name: "Log Graph",
+            description: "This is the third page",
+            children: [
+              {
+                name: "Sub-page 1",
+                description: "This is a sub-page of Page 3"
+              }
+            ]
+          },
+          {
+            name: "bar3",
+            description: "This is the fourth page",
+            children: [
+              {
+                name: "Sub-page 1",
+                description: "This is a sub-page of Page 4"
+              },
+              {
+                name: "Sub-page 2",
+                description: "This is another sub-page of Page 4"
+              }
+            ]
+          },
+          {
+            name: "Knowledge Repo",
+            description: "This is the fifth page",
+            children: [
+              {
+                name: "Sub-page 1",
+                description: "This is a sub-page of Page 5"
+              },
+              {
+                name: "Sub-page 2",
+                description: "This is another sub-page of Page 5"
+              }
+            ]
+          }
+        ]
+      };
+    },
+    methods: {
+      goToPage(page) {
+        this.childPage = null;
+        const index = this.pages.findIndex(p => p.name === page.name);
+        this.currentPage = index;
+      },
+      goToChildPage(childPage) {
+        const parentPage = this.pages[this.currentPage];
+        if (parentPage.children) {
+          this.childPage = childPage;
+        }
+        const index = parentPage.children.findIndex(p => p.name === childPage.name);
+        this.currentPage = index;
+      }
+    }
+  };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+  
+<style>
+.app {
+  max-width: 1200px;
+  margin: 0 auto;
+  background-color: #f7f7f7;
+  font-family: sans-serif;
 }
-ul {
-  list-style-type: none;
+  
+header {
+  background-color: #2684ff;
+  color: #fff;
+}
+  
+nav ul {
+  list-style: none;
+  display: flex;
+  margin: 0;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+  
+nav li {
+  margin-right: 20px;
+  padding: 10px;
+  cursor: pointer;
 }
-a {
-  color: #42b983;
+  
+nav li.active {
+  background-color: #135cb2;
+}
+  
+.sub-menu {
+  display: none;
+  position: absolute;
+  background-color: #2684ff;
+  margin-top: 10px;
+  padding: 10px;
+}
+  
+nav li:hover .sub-menu {
+  display: block;
+}
+  
+.sub-menu li {
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+  
+main {
+  padding: 20px;
+  background-color: #fff;
 }
 </style>
