6ac5bd96f4013161e06e58f7d8649cec1d4c9e95
JasonBourne1998 <3069696872@qq.com>
2023-06-18 20:26:23 +0800

    update overview

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import { createRouter, createWebHistory } from 'vue-router';
 import ElementPlus from 'element-plus';
 import 'element-plus/theme-chalk/index.css';
 import App from './App.vue';
-import FlowOverview from './components/FlowOverview.vue';
-import AttackChain from './components/AttackChain.vue';
+import Knowledg from './components/KnowledgeRepoSvchost.vue';
+import LogGraph from './components/LogGraph.vue';
+import RawLog from './components/LogAnalytics.vue'
+import SelectedLog from './components/LogProcess.vue'
+import AlertGraph from './components/AlertComponent.vue'
 
 const routes = [
-    { path: '/operation-1', component: FlowOverview },
-    { path: '/operation-2', component: AttackChain },
+    { path: '/operation-1', component: RawLog },
+    { path: '/operation-2', component: SelectedLog },
+    { path: '/operation-3', component: LogGraph },
+    { path: '/operation-4', component: Knowledg },
+    { path: '/operation-5', component: AlertGraph },
   // 其他路由配置
 ];
 
